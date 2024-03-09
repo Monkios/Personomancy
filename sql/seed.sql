dc2d8c17c201e36f2a4f91b9bd6b8d0aa06af73c
monkios <monkios777+github@gmail.com>
2024-03-09 01:10:38 -0500

    Remove config.php from repo

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,4 +1,3 @@
 -- Données préalables à l'utilisation de Mancy
 
-INSERT INTO `joueur` (`login`, `prenom`, `nom`, `courriel`, `salt`, `password`, `est_anim`, `active`, `date_insert`, `date_modify`) VALUES
-('Personnomancy', 'Personno', 'Mancy', '', '', '', '0', '0', '2024-03-04', '2024-03-04');
+INSERT INTO `joueur` (`prenom`, `nom`, `courriel`, `salt`, `password`) VALUES ('Personno', 'Mancy', '', '', '');
